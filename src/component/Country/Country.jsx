11c7f34ba8edf99981099eaa9162288f5cb9d833
Srabonti721 <somayaaktarshabonti721@gmail.com>
2025-04-07 00:01:44 +0600

    button togol

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
-    console.log(country.flags);
-    
+const Country = ({ country }) => {
+    const [vidited, setVisited] = useState(false);
+    // console.log(country.flags);
+
+    const handleVisited = () => {
+        // if (vidited === true) {
+        //     setVisited(false)
+        // }
+        // else {
+        //     setVisited(true)
+        // }
+        setVisited(!vidited)
+    }
+
     return (
-        <div className='common'>
+        <div className={`country ${vidited && 'country-visited'}`}>
             <h2>name : {country.name.common}</h2>
             <img src={country.flags.png} alt="" />
-            <p>independent : {country.independent? 'free':'not free'}</p>
+            <p>independent : {country.independent ? 'free' : 'not free'}</p>
             <p>population : {country.population}</p>
-          
+            <button className={vidited?'btn-visited':'btn-not-visited'} onClick={handleVisited}>{vidited ? 'Visited' : 'Not visited'}</button>
+
         </div>
     );
 };
